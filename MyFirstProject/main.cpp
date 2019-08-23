2cc14b31aa38b18686e716e947e61c3067ad2fff
daniel dooley <DoubleDD99@outlook.com>
2019-08-22 20:56:32 -0500

    MyFirstProject

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,15 @@
+/*
+	Daniel Dooley
+	C++ 1st Semester 2019
+	Due: Due Date 8/29/19
+	Lab number 1 Daniel Dooley
+	Output "Hello World" to the console.
+*/
+#include <iostream>
+using namespace std;
+
+int  main() {
+	system("color A1");
+	std::cout << "Hello World" << endl << "Your favorite color to the screen as well.";
+	return 0;
+}
